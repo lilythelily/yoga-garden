4c589757151c309201ed39cfca1b085751e8afa7
KaoriA <138273671+lilythelily@users.noreply.github.com>
2025-11-27 21:30:33 +0900

    initial commit

---
+++ details.php
@@ -0,0 +1,147 @@
+
+<?php
+
+$name = "Eternity Yoga Studio";
+$location = "Yokohama";
+$class = "Day & Night";
+$description = "Experienced Yoga instuctors walks you through from basic to advance."
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yoga Garden</title>
+    <link rel="stylesheet" href="style.css">
+
+    <nav>
+        <img src="./assets/icons/x-outline.svg" alt="close" class="close-btn">
+        <ul class="nav-ul">
+            <li>Home<img src="./assets/icons/chevron-right.svg" alt="chevron"></li>
+            <li>Search<img src="./assets/icons/chevron-right.svg" alt="chevron"></li>
+            <li>Online Classes<img src="./assets/icons/chevron-right.svg" alt="chevron"></li>
+      
+</ul>
+    </nav>
+</head>
+<body>
+
+<header class="details-header"><img src="./assets/logo.svg" alt="logo" class="header-logo">    <div class="input-icon">
+    <input type="text" class="search-field" placeholder="Search"><div class="search-icon"></div></div>
+<div class="search-icon-mobile"></div>
+<div class="hamburger"><img src="./assets/icons/menu-03.svg" alt="menu" class="hamburger-menu"></div>
+</header>
+
+<main>
+
+        <section class="section__details">
+
+        <div class="section__details-left">
+             <div class="section__details-img"></div>
+<div class="social-container">
+    <div class="social"><img src="./assets/icons/globe-03.svg" alt="globe"></div>
+    <div class="social"><img src="./assets/icons/Instagram - Original.svg" alt="instagram"></div>
+    <div class="social"><img src="./assets/icons/facebook.svg" alt="facebook"></div>
+    <div class="social"><img src="./assets/icons/email.svg" alt="email"></div>
+</div>
+
+        <div class="section__details-tags">
+            <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+        </div>
+
+        </div>
+
+        <div class="section__details-right">
+             <p class="section__details-title">Lotus Yoga Studio</p>
+             <div class="description-container">
+
+             <!-- item1 -->
+             <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/locator-gray.svg" alt="marker">Address</p>
+            <p class="section__details-desc">9538 Green Lane Wigan WN 12 6BW</p></div>
+          
+
+  <!-- item2 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/clock-01.svg" alt="clock">Opening Hours</p>
+            <p class="section__details-desc">08:00 - 21:00</p></div>
+
+
+  <!-- item3 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/calendar-02.svg" alt="calendar">Business Days</p>
+            <p class="section__details-desc">Tue to Sun</p></div>
+
+
+              <!-- item4 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/marker-01.svg" alt="marker">Level</p>
+            <p class="section__details-desc">Beginner Friendly</p></div>
+
+
+              <!-- item5 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/computer.svg" alt="computer">Online Class</p>
+            <p class="section__details-desc">Available</p></div>
+
+
+              <!-- item6 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/user-profile-02.svg" alt="profile">One to One</p>
+            <p class="section__details-desc">Available</p></div>
+
+
+              <!-- item7 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/currency-coin-pound.svg" alt="pound">Price/hour</p>
+            <p class="section__details-desc">From £15</p></div>
+
+
+              <!-- item8 -->
+                         <div class="icon-desc">
+            <p class="section__details-icon"><img src="./assets/icons/face-id-02.svg" alt="face">Free Trial</p>
+            <p class="section__details-desc">Available</p></div>
+
+
+             
+
+            <!-- ---- container ends ---- -->
+              </div>
+
+              <!-- instructor -->
+               <div class="instructor-wrapper">
+               <img src="./assets/instructor7.jpg" alt="avatar" class="instructor-img">
+               <div class="instructor-name">
+                <p class="instructor">Instructor</p>
+                <p class="instructor-name">Stacy Cho</p>
+               </div></div>
+           
+              <button type="button" class="details-trialBtn">Contact for a Free Trial<img src="./assets/icons/arrow-white.svg" alt="arrow"></button></section>
+        </div>
+
+           
+      
+
+
+
+    <!-- online class -->
+    <section class="section__online">
+        <div class="section__online-img"></div>
+        <div class="h3-btn">
+        <h3>Practice Yoga at Home?</h3>
+        <button type="button" class="online-btn">Browse Online Classes<img src="./assets/icons/arrow-green.svg" alt="arrow"></button></div>
+    </section>
+
+</main>
+
+<footer>
+    <img src="./assets/logo.svg" alt="logo" class="footer-logo">
+    <p class="copyright">©2025 Yoga Garden All Rights Reserved</p>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,121 @@
+
+<?php
+
+$name = "Eternity Yoga Studio";
+$location = "Yokohama";
+$class = "Day & Night";
+$description = "Experienced Yoga instuctors walks you through from basic to advance."
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yoga Garden</title>
+    <link rel="stylesheet" href="style.css">
+
+    <nav>
+        <img src="./assets/icons/x-outline.svg" alt="close" class="close-btn">
+        <ul class="nav-ul">
+            <li>Home<img src="./assets/icons/chevron-right.svg" alt="chevron"></li>
+            <li>Search<img src="./assets/icons/chevron-right.svg" alt="chevron"></li>
+            <li>Online Classes<img src="./assets/icons/chevron-right.svg" alt="chevron"></li>
+      
+</ul>
+    </nav>
+</head>
+<body>
+
+<header><img src="./assets/logo.svg" alt="logo" class="header-logo">
+<div class="hamburger"><img src="./assets/icons/menu-03.svg" alt="menu" class="hamburger-menu"></div>
+</header>
+   
+<div class="hero">
+    <h1>Find Your Perfect Yoga Class</h1>
+    <!-- <h2>Cultivate your <br>inner peace</h2> -->
+
+
+<div class="search-box">
+    <div class="input-icon">
+    <input type="text" class="location-search" placeholder="Search by Location"><div class="location-icon"></div><div class="search-icon"></div></div>
+
+    <div class="input2-icon">
+        <input type="text" class="keyword-search" placeholder="Search by Keyword"><div class="keyword-icon"></div><div class="search-icon2"></div></div>
+
+
+</div></div>
+
+<main>
+    <section class="section__cards">
+
+    <!-- card1 -->
+     
+        <div class="section__cards-item">
+            <a href="details.php">
+            <div class="section__cards-img"></div>
+            <p class="section__cards-title">Lotus Yoga Studio</p>
+            <div class="location-fee">
+            <p class="section__cards-location"><img src="./assets/icons/locator-gray.svg" alt="marker">Wigan</p>
+            <p class="section__cards-fee"><img src="./assets/icons/currency-coin-pound.svg" alt="pound">£15 / hour</p></div>
+
+        <div class="tags-container">
+            <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+        </div>
+
+        <button type="button" class="trial-btn">Try for Free<img src="./assets/icons/arrow-white.svg" alt="arrow"></button></div></a>
+
+        <!-- card 2 -->
+
+         <div class="section__cards-item">
+            <div class="section__cards-img"></div>
+            <p class="section__cards-title">Lotus Yoga Studio</p>
+            <div class="location-fee">
+            <p class="section__cards-location"><img src="./assets/icons/locator-gray.svg" alt="marker">Wigan</p>
+            <p class="section__cards-fee"><img src="./assets/icons/currency-coin-pound.svg" alt="pound">£15 / hour</p></div>
+
+        <div class="tags-container">
+            <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+        </div>
+
+        <button type="button" class="trial-btn">Try for Free<img src="./assets/icons/arrow-white.svg" alt="arrow"></button></div>
+
+
+        <!-- --card3-- -->
+         <div class="section__cards-item">
+            <div class="section__cards-img"></div>
+            <p class="section__cards-title">Lotus Yoga Studio</p>
+            <div class="location-fee">
+            <p class="section__cards-location"><img src="./assets/icons/locator-gray.svg" alt="marker">Wigan</p>
+            <p class="section__cards-fee"><img src="./assets/icons/currency-coin-pound.svg" alt="pound">£15 / hour</p></div>
+
+        <div class="tags-container">
+            <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+             <div class="tag-item"><img src="./assets/icons/check-green.svg" alt="check">Free Trial</div>
+        </div>
+
+        <button type="button" class="trial-btn">Try for Free<img src="./assets/icons/arrow-white.svg" alt="arrow"></button></div>
+    </section>
+
+
+    <!-- online class -->
+    <section class="section__online">
+        <div class="section__online-img"></div>
+        <div class="h3-btn">
+        <h3>Practice Yoga at Home?</h3>
+        <button type="button" class="online-btn">Browse Online Classes<img src="./assets/icons/arrow-green.svg" alt="arrow"></button></div>
+    </section>
+
+</main>
+
+<footer>
+    <img src="./assets/logo.svg" alt="logo" class="footer-logo">
+    <p class="copyright">©2025 Yoga Garden All Rights Reserved</p>
+</footer>
+
+</body>
+</html>
